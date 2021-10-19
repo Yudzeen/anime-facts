4f1ce1c05f79b59c248396ecf8ec6d361536bca4
Eugene Javiñas <eugenejavinas@gmail.com>
2021-10-19 17:54:02 +0800

    Initial commit

---
+++ app/src/main/java/ebj/habinyasuyujin/animefacts/MainApplication.kt
@@ -0,0 +1,7 @@
+package ebj.habinyasuyujin.animefacts
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication: Application()
---
+++ app/src/main/java/ebj/habinyasuyujin/animefacts/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package ebj.habinyasuyujin.animefacts.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ebj.habinyasuyujin.animefacts.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
